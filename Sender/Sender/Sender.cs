fade5d13b8a9b978f91403aa130bf4744874d5fa
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-09-15 14:50:13 +0530

    Sender Complexity Reduced

--- Sender/Sender/Sender.cs
+++ Sender/Sender/Sender.cs
@@ -9,59 +9,130 @@ namespace Sender
         {
             //Console.WriteLine("Enter File path");
             //string path = Console.ReadLine();
-            string path = "C:/Users/HP/Desktop/CaseStudy_Review/review-case-s22b7/sample-review/review-report.csv";
-            if (File.Exists(path))
+            string path = "C:/Users/HP/Desktop/CaseStudyFinal/review-case-s22b7/sample-review/review-report.csv";
+            bool pathExists = PathExistance.CheckPathExistOrNot(path);
+            if (pathExists == true)
             {
-                //Console.WriteLine("Specified file exists.");
-                Console.WriteLine("Enter Line Number to get word series of perticular line and enter 0 to get word series of complete csv file");
-                String LineNumber = Console.ReadLine();
-                SeriesOfWords.ConvertCommentsToSeriesOfWords(path, LineNumber);
-            }
-            else
-            {
-                Console.WriteLine("File not found");
+                Console.WriteLine("Enter 0 to get Series of Words for complete CSV file and Enter 1 for Column filter");
+                String UserInput = Console.ReadLine();
+                if (UserInput == "0")
+                {
+                    SeriesOfWords.ConvertCommentsToSeriesOfWords(path, "0");
+                }
+                else
+                {
+                    string ColumnNumber = Column_Number.TakeColumnNumberForColumnFilter(UserInput);
+                    SeriesOfWords.ConvertCommentsToSeriesOfWords(path, ColumnNumber);
+                }
             }
         }
     }
-    class SeriesOfWords
+    public class PathExistance
     {
-        public static void ConvertCommentsToSeriesOfWords(string path, string user_input)
+        public static bool CheckPathExistOrNot(string path)
         {
-            int userinput = Convert.ToInt32(user_input);
-            string[] lines = System.IO.File.ReadAllLines(path);
-            int startline, stopline;
-            if (userinput != 0)
+            if (File.Exists(path))
             {
-                startline = userinput;
-                stopline = userinput + 1;
+                return true;
             }
             else
             {
-                startline = 1;
-                stopline = lines.Length;
-
+                Console.WriteLine("File Not Found");
+                return false;
             }
-            for (int lineNumber = startline; lineNumber < stopline; lineNumber++)
+        }
+    }
+
+    class Column_Number
+    {
+        public static string TakeColumnNumberForColumnFilter(string UserInput)
+        {
+            Console.WriteLine("Enter 1 for Date & Time column filter and 2 for Comments Column filter");
+            String ColumnNumber = Console.ReadLine();
+            return ColumnNumber;
+        }
+
+    }
+
+    class SeriesOfWords
+    {
+        public static void ConvertCommentsToSeriesOfWords(string path, string Column_Number)
+        {
+            int column_number = Convert.ToInt32(Column_Number);
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            for (int lineNumber = 1; lineNumber < lines.Length; lineNumber++)
             {
                 string line = lines[lineNumber];
                 string[] columns = line.Split(',');
+                int columnsCount = columns.Length;
+                int[] Start_StopColumnNumber = StartStopColumnNumbers.ReturnStartAndStopColumnNumbers(column_number, columnsCount);
+                int StartColumnNumber = Start_StopColumnNumber[0];
+                int StopColumnNumber = Start_StopColumnNumber[1];
+                Column_Filter.ApplyColumnFilter(line, StartColumnNumber, StopColumnNumber);
+            }
+        }
+    }
 
-                for (int columnNumber = 0; columnNumber < columns.Length; columnNumber++)
+    class StartStopColumnNumbers
+    {
+        public static int[] ReturnStartAndStopColumnNumbers(int ColumnNumber, int ColumnCount)
+        {
+            int[] start_stopColumnNumber = new int[2];
+            if (ColumnNumber == 0)
+            {
+                start_stopColumnNumber[0] = 0;
+                start_stopColumnNumber[1] = ColumnCount;
+                return start_stopColumnNumber;
+            }
+            else if (ColumnNumber == 1)
+            {
+                start_stopColumnNumber[0] = 0;
+                start_stopColumnNumber[1] = 1;
+                return start_stopColumnNumber;
+            }
+            else
+            {
+                start_stopColumnNumber[0] = 1;
+                start_stopColumnNumber[1] = ColumnCount;
+                return start_stopColumnNumber;
+            }
+        }
+    }
+
+    class Column_Filter
+    {
+        public static void ApplyColumnFilter(string line, int startcolumn, int stopcolumn)
+        {
+            string[] columns = line.Split(',');
+            for (int columnNumber = startcolumn; columnNumber < stopcolumn; columnNumber++)
+            {
+                string reviewComment = columns[columnNumber];
+
+                string[] words = reviewComment.Split(' ');
+
+                foreach (var word in words)
                 {
-                    string reviewComment = columns[columnNumber];
-                    //Console.WriteLine(reviewComment);
-
-                    string[] words = reviewComment.Split(' ');
-                    foreach (var word in words)
-                    {
-                        //System.Console.Write(word+"  ");
-                        System.Console.WriteLine(word);
-                    }
-                    Console.WriteLine();
-
+                    System.Console.WriteLine(word);
                 }
             }
+        }
+    }
 
+    // test path existance
+    public class PathExistanceOfCsvFile
+    {
+        public bool CheckForPathExistOrNot(string path)
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+                return false;
+            }
         }
     }
 }
--- Sender/Sender_Test/Sender_test.cs
+++ Sender/Sender_Test/Sender_test.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
+using Sender;
 
 namespace Sender_Test
 {
-    class Sender_test
+    public class Sender_test
     {
         static void Main(string[] args)
         {
+        }
+        [Fact]
+        public void TestCsvFileExists()
+        {
+            PathExistanceOfCsvFile pathchecker = new PathExistanceOfCsvFile();
+            string path = "C:/Users/HP/Desktop/CaseStudyFinal/review-case-s22b7/sample-review/review-report.csv";
+            Assert.True(pathchecker.CheckForPathExistOrNot(path));
+            // Assert.False(pathchecker.CheckForPathExistOrNot("C:/Users/HP/Desktop/CaseStudyFinal/sample-review/review-report.c"));
+
         }
     }
 }
