7118bf26e9f3c2d56063fb2f34ce649586d53f28
Adarsh S Srivatsa <adarsh.98srivatsa@gmail.com>
2020-09-18 17:10:28 +0530

    resharper report (#4)
    * resharper report
    
    * report

--- Receiver/Receiver.Test/ReceiverTestSuite.cs
+++ Receiver/Receiver.Test/ReceiverTestSuite.cs
@@ -7,42 +7,42 @@ namespace Receiver.Test
 {
     public class ReceiverTestSuite
     {
-        ReadConsoleHandler reader = new ReadConsoleHandler();
-        CsvWriteHandler writer = new CsvWriteHandler();
-        string fileName = "Example.csv";
-        Dictionary<string, int> wordmap = new Dictionary<string, int>() { { "Hello", 2 }, { "Philips", 1 }, { "World", 2 } };
+        readonly ReadConsoleHandler _reader = new ReadConsoleHandler();
+        readonly CsvWriteHandler _writer = new CsvWriteHandler();
+        readonly string _fileName = "Example.csv";
+        readonly Dictionary<string, int> _wordmap = new Dictionary<string, int>() { { "Hello", 2 }, { "Philips", 1 }, { "World", 2 } };
 
         [Fact]
         public void GivenListOfLinesThenReturnsWordCountMap()
         {
             List<String> testList = new List<String>() { "Hello", "World", "Philips", "Hello", "Philips", "and" };
-            Dictionary<String, int> outcome = reader.ListToWordCountMap(testList);
-            Assert.Equal(wordmap["Hello"], outcome["Hello"]);
+            Dictionary<String, int> outcome = _reader.ListToWordCountMap(testList);
+            Assert.Equal(_wordmap["Hello"], outcome["Hello"]);
         }
         [Fact]
         public void WhenFileArgumentIsHealthyThenCreateNewFile()
         {
-            writer.FilePathGen(fileName);
-            Assert.True(File.Exists(GetPath(fileName)));
-            Assert.Equal(0, new FileInfo(GetPath(fileName)).Length);
+            _writer.FilePathGen(_fileName);
+            Assert.True(File.Exists(GetPath(_fileName)));
+            Assert.Equal(0, new FileInfo(GetPath(_fileName)).Length);
         }
         [Fact]
         public void WhenFileisCorruptedThenThrowException()
         {
-            Assert.Throws<ApplicationException>(() => writer.FilePathGen(""));
+            Assert.Throws<ApplicationException>(() => _writer.FilePathGen(""));
         }
 
         [Fact]
         public void WhenFileStreamFailsThenThrowException()
         {
-            Assert.Throws<ApplicationException>(() => writer.WriteToCsv(wordmap, ""));
+            Assert.Throws<ApplicationException>(() => _writer.WriteToCsv(_wordmap, ""));
         }
 
         [Fact]
         public void WhenFileStreamSucceedsThenWriteOnFile()
         {
-            writer.WriteToCsv(wordmap, GetPath(fileName));
-            Assert.Equal("Hello,2", File.ReadAllLines(GetPath(fileName))[0]);
+            _writer.WriteToCsv(_wordmap, GetPath(_fileName));
+            Assert.Equal("Hello,2", File.ReadAllLines(GetPath(_fileName))[0]);
         }
         private static string GetPath(string fileName)
         {
